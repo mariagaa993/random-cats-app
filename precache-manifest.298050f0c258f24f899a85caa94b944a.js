3118a7e97b7f35a47bb11222ae5fb791683fc786
Maria Alarcon <mariagaa993@gmail.com>
2020-10-01 00:53:54 -0400

    Updates

---
+++ precache-manifest.298050f0c258f24f899a85caa94b944a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27ffaaefe54709fe8864dd32eaa8688",
+    "url": "/random-cats-app/index.html"
+  },
+  {
+    "revision": "bc36689de30ddc442d4a",
+    "url": "/random-cats-app/static/css/main.c71b3349.chunk.css"
+  },
+  {
+    "revision": "a808cd931192de288070",
+    "url": "/random-cats-app/static/js/2.bdb867e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-cats-app/static/js/2.bdb867e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc36689de30ddc442d4a",
+    "url": "/random-cats-app/static/js/main.c62066bc.chunk.js"
+  },
+  {
+    "revision": "6725ab1a5e94b2434cac",
+    "url": "/random-cats-app/static/js/runtime-main.9ff2af21.js"
+  }
+]);
